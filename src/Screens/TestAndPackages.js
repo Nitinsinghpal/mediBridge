2a9ec620c33ec9a1ac8ad163967808ca1a11a88d
Nitin <nitinsingh919293@gmail.com>
2023-08-29 09:43:42 +0530

    added condition for testAndPackages

--- src/Screens/TestAndPackages.js
+++ src/Screens/TestAndPackages.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, Routes,Route } from 'react-router-dom'
 import Lifestyle from './Lifestyle'
 import Conditions from './Conditions'
 import Alphabet from './Alphabet'
 
 function TestAndPackages() {
-    function handleChange(params) {
-        alert(params)
+  const[showLifestyle, setshowLifestyle] = useState(true)
+  const[showConditions,setshowConditions] = useState(false)
+  const[showAlphabet,setshowAlphabet] = useState(false)
+
+    function handleChange(e) {
+      if (e.target.name === "lifestyle") {
+        setshowLifestyle(true)
+        setshowConditions(false)
+        setshowAlphabet(false)
+      }else if (e.target.name === "condition") {
+        setshowConditions(true)
+        setshowAlphabet(false)
+        setshowLifestyle(false)
+      }else if (e.target.name === "alphabet") {
+        setshowAlphabet(true)
+        setshowLifestyle(false)
+        setshowConditions(false)
+      }
     }
   return (
     <>
@@ -14,7 +30,14 @@ function TestAndPackages() {
     <h1>TestAndPackages</h1>
     <div>
     
-    <button onClick={handleChange}>lifestyle Disorder</button>
+    <button name='lifestyle' onClick={handleChange}>lifestyle Disorder</button>
+    <button name='condition' onClick={handleChange}>Conditions</button>
+    <button name='alphabet' onClick={handleChange}>Alphabet</button>
+    {showLifestyle && <Lifestyle/>}
+    {showAlphabet && <Alphabet/>}
+    {showConditions && <Conditions/>}
+
+    {}
         <hr/>
         
         {/* <Link to='/lifestyle'>lifestyle Disorder</Link>
